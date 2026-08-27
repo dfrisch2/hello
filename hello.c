801dbbb6e7b209b88d6df30ebab6f25f1d7ca278
Devin Frisch <dfrisch2@huskers.unl.edu>
2026-08-27 09:49:47 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Devin Frisch
+ * Date: 08/27/2026
+ *
+ * A simple program that will print a hello and my name
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char **argv) {
+
+    printf("Devin Frisch\n");
+
+    return 0;
+}
